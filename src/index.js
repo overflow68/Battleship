8046297939c00bc2948ff663ddb120e9010d98ab
Gabriel Nicolitoiu <nicolitoiu.gabriel23@gmail.com>
2021-08-28 17:36:36 +0100

    UI changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import style from './style.css';
 import {game} from './game.js';
-import {boardFactory} from './gameboard.js'
-import {currentShip} from './gameboard.js'
+
 
 
 let playerBoard = document.getElementById("playerBoard");
